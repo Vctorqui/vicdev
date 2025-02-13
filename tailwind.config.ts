4ac982a629a2c3217c37e1e0fca39d0e69b1c152
Víctor Quiñones <victhorq716@gmail.com>
2025-02-13 16:43:44 -0400

    add daisyui dependency and enhance IntroCard with tooltips

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -66,23 +66,27 @@ const config = {
           from: { height: "var(--radix-accordion-content-height)" },
           to: { height: "0" },
         },
-        "appear": {
-           "0%": {
-             opacity: "0",
-           },
-           "100%": {
-             opacity: "1"
-           }
-           }
+        appear: {
+          "0%": {
+            opacity: "0",
+          },
+          "100%": {
+            opacity: "1",
+          },
+        },
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
-        "appear": "appear 0.5s ease-in-out"
+        appear: "appear 0.5s ease-in-out",
       },
     },
   },
-  plugins: [require("tailwindcss-animate"), require("@tailwindcss/typography")],
+  plugins: [
+    require("tailwindcss-animate"),
+    require("@tailwindcss/typography"),
+    require("daisyui"),
+  ],
 } satisfies Config;
 
 export default config;
